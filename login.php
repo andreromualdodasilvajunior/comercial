e58572fc75f4dad0f6239c8870e60b14dc4372f7
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-12 20:29:28 -0300

    Para mensagem de erro

--- login.php
+++ login.php
@@ -9,5 +9,9 @@ $senha = $_POST["senha"];
  if (strlen($usuario) < 4 || strlen($usuario) > 15) {
         $mensagem = "Erro: o nome de usuário deve ter entre 4 e 15 caracteres.";
     }
-
+  else if (strlen($senha) < 4 || strlen($senha) > 15) {
+        $mensagem = "Erro: a senha deve ter entre 4 e 15 caracteres.";
+    }
+ 
+    else {
 
