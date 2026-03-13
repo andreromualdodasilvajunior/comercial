7fa51fce753abbb6b2faba20d455283783bed75f
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-13 19:38:32 -0300

    Inicio html

--- login.php
+++ login.php
@@ -39,3 +39,15 @@ if ($_SERVER["REQUEST_METHOD"] === 'POST') {
         }
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
