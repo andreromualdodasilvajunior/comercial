cfb3553388fdbe1750cc667584a22f57995fef94
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-12 21:20:51 -0300

    Corrigindo erro

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
-    <?php
-    $mensagem = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+<?php
+$mensagem = "";
+ 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
