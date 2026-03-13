8279f9e0c99517126b8125c41e22f28b75a820af
andreromualdodasilvajunior <andreromualdo063@gmail.com>
2026-03-13 19:15:16 -0300

    Atualizando novamente

--- login.php
+++ login.php
@@ -3,3 +3,10 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 $mensagem = '';
+
+if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+
+    $usuario = trim($_POST['usuario'] ?? '');
+    $senha   = trim($_POST['senha']   ?? '');
+
+    
